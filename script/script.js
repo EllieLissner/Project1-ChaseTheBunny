0dbb935ff7287569336d82f97c615afd1f1fd150
Ellie-Lissner <eswalker0723@gmail.com>
2021-05-14 18:13:53 -0500

    added css, js, html, and added event listener for specific keystroke

---
+++ script/script.js
@@ -0,0 +1,12 @@
+window.addEventListener("DOMContentLoaded", () => {
+    let test = document.querySelector(".test")
+    
+   //def use keydown for dog movement. 
+    document.addEventListener('keydown', (event => {
+        if (event.key === 'w') {
+            console.log("hello")
+        }
+    }))
+
+   
+})
