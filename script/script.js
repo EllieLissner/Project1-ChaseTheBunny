12e56e5281c4741d2ba1807dd28e3ef9619c2be3
Ellie-Lissner <eswalker0723@gmail.com>
2021-05-15 11:14:42 -0500

    made div move up and down with pressed keystroke

--- script/script.js
+++ script/script.js
@@ -1,13 +1,29 @@
 window.addEventListener("DOMContentLoaded", () => {
     let test = document.querySelector(".test")
+    let y = 10
     
-   //def use keydown for dog movement. 
-    document.addEventListener('keydown', (event => {
-        let x = 100
-        if (event.key === 'w') {
-            test.style.left = x + 'px'
+    function move (event) {
+      console.log(event)
+        if (event.key === 's') {
+            y += 10
+            test.style.top = y + 'px'
+            console.log("moving down")
+        } else if (event.key === 'w') {
+            y -= 10
+            test.style.top = y + 'px'
+            console.log("moving up")
         }
-    }))
+    }
+   
+   //def use keydown for dog movement. 
+    document.addEventListener('keydown', (event) => {
+        let x = setTimeout(() => {
+            move(event)
+           }, 5)
+        
+        
+    } )
 
+    
    
 })
